ea64a792763b9da72e5f30ee15f7a5aab352b30c
Beauty Gededzha <Beauty.Khuliso@gmail.com>
2026-04-19 19:11:03 +0200

    Update sql queries.sql

--- data processing/sql queries.sql
+++ data processing/sql queries.sql
@@ -1,3 +1,31 @@
+select * from `workspace`.`viewship`.`viewership limit 100;
+outter join from `workspace`.`userprofiles`.`user_profiles'
+
+   --1. all Viewership columns
+ SELECT
+ v.UserID0,
+ v.Channel2,
+ v.RecordDate2,
+ v'Duration 2',
+
+ ----2. All user profiles
+ u.Name,
+ u.surname,
+ u.Email,
+ u.Gender,
+ u.Race,
+ u.age,
+ u. province,
+ u.'Social media handle',
+-----------------------------
+   3. classfication of age
+ CASE
+ WHEN u.age >50 THEN 'Senior'
+ WHEN u.age BETWEEN 30 AND 49 THEN 'Adult'
+WHEN u.age BETWEEN 20 AND 29 THEN 'Young_Adult'
+WHEN u.age BETWEEN 12 AND 19 THEN 'Teenager'
+ELSE 'Child'
+END AS Age_classification,
 -----------------------------------------------------------
 ----most channel watched
 SELECT v.channel2, COUNT(v.channel) AS most_watched_channel
@@ -24,3 +52,26 @@ END AS duration_category
 FROM `workspace`.`viewship`.`viewership` v
 LEFT JOIN `workspace`.`userprofiles`.`user_profiles` u
 ON v.UserID = u.UserID
+province where BrightTV is watched
+SELECT u.province, COUNT(u.province) AS Total_Povinces
+FROM  `workspace`.`viewship`
+left join `workspace`.`userprofiles`
+on v.UserID = u.UserID;
+GROUP BY u. province;
+
+-------------------------------------------------------------------------------
+----Times and dates in the dataset are supplied in UTC and should be converted to SA
+time.
+
+Select from _utc_ timestamp (v.'recorddate2','Africa/johannesburg')AS SA_converted_time
+From `workspace`.`viewship`.`viewership` v
+
+------------------------------------------------------
+-----time took to watch the channel program
+Date_FORMAT(v.RecordDate2, 'yyyy-MM-dd')AS DATE,
+Date_FORMAT(v.RecordDate2, 'HH:mm:ss')AS TIME
+FROM `workspace`.`viewship`.`viewership` v
+Left JOIN `workspace`.`userprofiles`.`user_profiles` u
+on v.UserID = u.UserID;
+------------------------------------------
+
